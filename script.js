bab51d10e60bace6bb1e352095b67baf680c0815
JulianaMoraesLbn <juliana.moraes@labenu.com.br>
2023-11-06 22:23:12 -0300

    aula de variaveis com a telks

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 
+/* const nome = "Juliana"
+let idade = 36
+let presenca = true
+
+console.log(typeof nome, nome)
+console.log(typeof idade, idade)
+console.log(typeof presenca, presenca) */
+
+//-----
+
+//nome = "Fernanda"
+
+/* idade = 37
+console.log(idade);
+
+presenca = false
+console.log(presenca); */
+
+// ----------
+
+const nome = prompt("Informe o seu nome")
+let idade = Number(prompt("Informe sua idade"))
+let presenca = confirm("Está presente na aula?")
+
+console.log(typeof nome, nome)
+console.log(typeof idade, idade)
+console.log(typeof presenca, presenca)
+
+//-----
+
+console.log("Estudante", nome, "tem", idade, "anos. Presença:", presenca)
+
+console.log("Estudante" + nome + "tem" + idade + "anos. Presença:" + presenca)
+
+console.log("Nome: ", typeof nome, "Idade: ", typeof idade, "Presença: ", typeof presenca)
+
